740a77eb3bb4eb0839778230de08959a9d92f0d0
Areeb <areeb7676@gmail.com>
2020-06-12 23:46:10 +0500

    homescreen layout

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:aria_makeup/shared/Constants.dart';
 import 'package:flutter/material.dart';
 import 'package:aria_makeup/services/AuthenticateService.dart';
 import 'package:aria_makeup/screens/UserDataScreen.dart';
@@ -45,15 +48,186 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(
           color: Colors.white,
           child: Column(
-            children: <Widget>[titleBar(), trendingDrawer()],
+            children: <Widget>[
+              titleBar(),
+              trendingDrawer(),
+              homeScreenBottomDrawer()
+            ],
           ),
         ));
   }
 
+  var homeScreenOptionState = 0;
+  void homeScreenOptionStateHandler(int selected) {
+    setState(() {
+      homeScreenOptionState = selected;
+    });
+  }
+
+  bool homeScreenOptionStateCheck(var id) {
+    return id == homeScreenOptionState;
+  }
+
+  Widget homeScreenBottomDrawer() {
+    return Expanded(
+      child: Container(
+        // padding: EdgeInsets.only(left: 20),
+        child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(30)),
+              color: mainThemeColor,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        homeScreenOptionStateHandler(0);
+                      },
+                      child: Text(
+                        "Collection",
+                        style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                                decorationThickness: 2.5,
+                                decoration: homeScreenOptionStateCheck(0)
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                                fontWeight: homeScreenOptionStateCheck(0)
+                                    ? FontWeight.w600
+                                    : FontWeight.normal,
+                                color: Colors.white,
+                                fontSize:
+                                    homeScreenOptionStateCheck(0) ? 18 : 15)),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        homeScreenOptionStateHandler(1);
+                      },
+                      child: Text(
+                        "New",
+                        style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                                decorationThickness: 2.5,
+                                decoration: homeScreenOptionStateCheck(1)
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                                fontWeight: homeScreenOptionStateCheck(1)
+                                    ? FontWeight.w600
+                                    : FontWeight.normal,
+                                color: Colors.white,
+                                fontSize:
+                                    homeScreenOptionStateCheck(1) ? 18 : 15)),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        homeScreenOptionStateHandler(2);
+                      },
+                      child: Text(
+                        "Collection",
+                        style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                                decorationThickness: 2.5,
+                                decoration: homeScreenOptionStateCheck(2)
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                                fontWeight: homeScreenOptionStateCheck(2)
+                                    ? FontWeight.w600
+                                    : FontWeight.normal,
+                                color: Colors.white,
+                                fontSize:
+                                    homeScreenOptionStateCheck(2) ? 18 : 15)),
+                      ),
+                    ),
+                  ],
+                ),
+                bottomDrawer()
+              ],
+            )),
+      ),
+    );
+  }
+
+  Widget bottomDrawer() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.only(top: 10, bottom: 30),
+        margin: EdgeInsets.only(left: 10),
+        // color: Colors.red,
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(
+              width: 20,
+            ),
+            bottomDrawerCard(Colors.orange),
+            SizedBox(
+              width: 20,
+            ),
+            bottomDrawerCard(Colors.greenAccent),
+            SizedBox(
+              width: 20,
+            ),
+            bottomDrawerCard(Colors.blue),
+            // trendingCard(Colors.green),
+            SizedBox(
+              width: 20,
+            ),
+            bottomDrawerCard(Colors.yellow),
+            // trendingCard(Colors.yellow),
+            SizedBox(
+              width: 20,
+            ),
+            bottomDrawerCard(Colors.red)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bottomDrawerCard(var col) {
+    var tileWidth = MediaQuery.of(context).size.width / 3;
+    var bottom = MediaQuery.of(context).viewInsets.bottom;
+    // print(bottom.to+'here');
+    var tileHeight = tileWidth * 1.42;
+    return Container(
+      width: tileWidth,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: col),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Lorem Ipsum",
+            style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    fontSize: 15)),
+          )
+        ],
+      ),
+    );
+  }
+
   Widget trendingDrawer() {
+    final trendingCardHeigth = 300.0;
     return Container(
       margin: EdgeInsets.symmetric(vertical: 20.0),
-      height: 250.0,
+      height: trendingCardHeigth,
       child: ListView(
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
@@ -61,9 +235,7 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(
             width: 20,
           ),
-          trendingCard(Colors.red)
-          ,
-          
+          trendingCard(Colors.red),
           SizedBox(
             width: 20,
           ),
@@ -71,16 +243,13 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(
             width: 20,
           ),
-          Container(
-            width: 160.0,
-            color: Colors.green,
-          ),
+          trendingCard(Colors.green),
           SizedBox(
             width: 20,
           ),
-          Container(
-            width: 160.0,
-            color: Colors.yellow,
+          trendingCard(Colors.yellow),
+          SizedBox(
+            width: 10,
           ),
         ],
       ),
@@ -88,14 +257,43 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget trendingCard(var col) {
+    Color trendingCardNameColor = Color.fromRGBO(0, 0, 0, 0.6);
     return Container(
-      width: 200,
+      height: 300,
+      width: 180,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            height: 100,
-            color: col,
-          )
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              color: col,
+            ),
+            height: 240,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Lorem Ipsum",
+            style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: trendingCardNameColor,
+                    fontSize: 15)),
+          ),
+          Text(
+            "Rs. 580",
+            style: GoogleFonts.montserrat(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: trendingCardNameColor,
+                  fontSize: 20,
+                ),
+                color: mainThemeColor),
+          ),
         ],
       ),
     );
